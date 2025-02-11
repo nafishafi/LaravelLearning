e7dcee50d0802e67fb8e865d6bfcda50976a08d4
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 14:35:18 +0700

    add exercise 4

---
+++ Php_Basic/exc4_typedata_string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    /*
+        Tes
+        Halo
+    */
+    
+    // variabel pada php
+    $nama = "Sekolah Koding 123";
+    $nama2 = "Hilman";
+
+    echo $nama . " untuk " . $nama2;
+    ?>
+</body>
+</html>
