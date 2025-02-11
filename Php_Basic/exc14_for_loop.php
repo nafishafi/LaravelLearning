4e1faf41598ad6f8bfc22fb2dbb095219bd5bb9f
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 16:37:56 +0700

    add exercise 14

---
+++ Php_Basic/exc14_for_loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- For Loop -------
+        // for(variabel awal, batas, perubahan)
+        
+        for($i = 0; $i < 5; $i++){
+            echo "sekolah koding <br>";
+        }
+    ?>
+</body>
+</html>
