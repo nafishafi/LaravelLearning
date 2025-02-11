e2e34bd0bd8bf8f6bb905f52dcca59f78ad9ca1d
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 14:49:47 +0700

    add exercise 6

---
+++ Php_Basic/exc6_operation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+        // operator aritmatik
+        // + - * / % ++ --
+        $angka = 1000;
+        $angka2 = 3;
+        $angka3 = 2;
+
+        $angka++; // + 1
+        $angka--; // - 1
+
+        // $angka += $angka2
+
+        echo $angka + $angka2 * $angka3;
+    ?>
+</body>
+</html>
