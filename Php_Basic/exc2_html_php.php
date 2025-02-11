6f9b3addeaa88a1cc412bcdcc653443077efb090
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 14:20:48 +0700

    add exercise 2

---
+++ Php_Basic/exc2_html_php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Sekolah Koding</h1>";
+        echo "Halo";
+        echo " Member"; // spasi berapapun akan di konversi menjadi 1
+    ?>
+
+    Halo semuanya!
+</body>
+</html>
