a6f4e68a8aa6dc77bff3bb74c0e6dab090cf5e7e
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 14:28:32 +0700

    add exercise 3

---
+++ Php_Basic/exc3_comment_variable.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    /*
+        Tes
+        Halo
+    */
+    
+    // variabel pada php
+    $x = "Sekolah Koding 123";
+    $_nama = "Hilman";
+
+    echo "<h1>$x</h1>";
+    echo "Halo ". $x . "<br>";
+    echo " Member"; // spasi berapapun akan di konversi menjadi 1
+    
+    ?>
+
+    Halo semuanya!
+</body>
+</html>
