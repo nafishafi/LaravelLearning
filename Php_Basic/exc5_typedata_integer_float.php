62cc04fb5650f9e7c1dbafac4559f11fc936fd6c
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-11 14:40:32 +0700

    add exercise 5

---
+++ Php_Basic/exc5_typedata_integer_float.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    /*
+        Tes
+        Halo
+    */
+    
+    // variabel pada php
+    $angka = 1000;
+    $angka2 = 100.5;
+    echo "angka adalah $angka <br>";
+    echo $angka + $angka2;
+    ?>
+</body>
+</html>
