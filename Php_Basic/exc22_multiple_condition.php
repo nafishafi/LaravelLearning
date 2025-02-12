fb8b2e6c8cb417df4e19642fcffc04fc66bad7fc
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 10:31:09 +0700

    add exercise 22

---
+++ Php_Basic/exc22_multiple_condition.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- If and else -------
+        // operator logika == === > >= < <= !=
+        // === -> tidak hanya 3 sama dengan tetapi type data nya juga
+        $uang_programmer = 1000;
+        $keyboard = 2000;
+        $uang_designer = 3000;
+
+        if($uang_programmer > $keyboard){
+            echo 'dibeli';
+        }else if($uang_designer > $keyboard){
+            echo 'dibeli';
+        }else{
+            echo 'ngga cukup';
+        }
+
+    ?>
+</body>
+</html>
