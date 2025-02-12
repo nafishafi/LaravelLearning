c03a89572bd02d48ab071c8e02adda6fac1ed37b
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 11:10:48 +0700

    add exercise 31

---
+++ Php_Basic/exc31_anonymus_function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ---- anonymous function ----
+        $ngetik = function($text){
+            echo $text;
+        };
+        
+        $ngetik('selamat datang semuanya');
+        
+    ?>
+</body>
+</html>
