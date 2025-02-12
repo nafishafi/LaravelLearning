a8ba4033776d2a21041eaf19381c1538f9daf43a
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 11:37:34 +0700

    add exercise 34

---
+++ Php_Basic/exc34_die_var_dump.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=z, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+    <?php
+        // echo print
+        // print_r die var_dump
+        $car = 'mobil';
+        var_dump($car); // debug
+        die($car); // menghentikan script, hanya dijalankan sampai disini  
+        echo 'satu dua tiga'; 
+    ?>
+</body>
+</html>
