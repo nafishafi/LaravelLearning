df7401b3daef9b63910ec22e19918d35400bad20
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 10:51:12 +0700

    add exercise 26

---
+++ Php_Basic/exc26_switch_case.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- Switch Case -------
+        $nama = "Hilman";
+
+        switch ($nama) {
+            case 'Endy':
+                echo 'namanya endy';
+                break;
+            case 'Tiqa':
+                echo 'namanya Tiqa';
+                break;
+            case 'Hilman':
+                echo 'namanya Hilman';
+                break;
+            default:
+                echo 'tidak ada yang benar';
+        }
+        
+    ?>
+</body>
+</html>
