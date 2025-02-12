30e1e2b5f4b698661f6c158bd4f589307ef199f2
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 10:53:09 +0700

    add exercise 27

---
+++ Php_Basic/exc27_function.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- Function -------
+        function panggil(){
+            echo "--------";
+            echo "Sekolah Ngoding";
+            echo "--------";
+        }
+        panggil();
+        
+    ?>
+</body>
+</html>
