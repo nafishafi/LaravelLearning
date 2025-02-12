a25766e1885ba5a2457d2fae537e64e7cf5cd3ad
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 10:14:08 +0700

    add exercise 20

---
+++ Php_Basic/exc20_if_else.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- If and else -------
+        $password = '123';
+        $password2 = '1234';
+
+        if($password = $password2){
+            echo 'anda berhasil masuk!';
+        } else {
+            echo 'gagal! passwordnya salah';
+        }
+    ?>
+</body>
+</html>
