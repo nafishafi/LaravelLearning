2724bba6234661906e3a800720201801a11b7fd4
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-12 10:57:06 +0700

    add exercise 28

---
+++ Php_Basic/exc28_parameter.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Array PHP</h1>
+    <?php
+        // ------- Function -------
+
+use function Laravel\Prompts\text;
+
+        function print_text($text, $angka){
+            $text = "Latihan " . $text . " " . $angka;
+            echo "--------";
+            echo $text;
+            echo "--------";
+        }
+        print_text("Tes Ngoding", 90);
+        
+    ?>
+</body>
+</html>
