af82375a724ab6bac34764dd9be723093dd7317f
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-13 10:29:36 +0700

    add exercise 4

---
+++ Php_Oop/class_robot.php
@@ -0,0 +1,31 @@
+<?php 
+    // class
+    class robot{
+        //property
+        public $suara;
+        public $berat;
+
+        //konstruktor
+        public function __construct($suara, $berat)
+        {
+            $this->suara = $suara;
+            $this->berat = $berat;
+        }
+
+        //metode set (mengoper parameter dan mengubah nilai dari property) && metode get
+        public function set_suara($suara1){ // set setting
+            $this->suara = $suara1;
+        }
+        public function set_berat($berat1){ // set setting
+            $this->berat = $berat1;
+        }
+
+        //method
+        public function get_suara(){ // get mengambil
+            return $this->suara;
+        }
+        public function get_berat(){ // get mengambil
+            return $this->berat;
+        }
+    }
+?>
---
+++ Php_Oop/exc4_constructor.php
@@ -0,0 +1,11 @@
+<?php 
+    require_once 'class_robot.php';
+    
+    $robot1 = new robot('ngik ngik ngok', 20);
+    $robot2 = new robot('titut', 30);
+
+    $robot1->set_berat(50);
+    
+    echo 'beratnya.. ' . $robot1->get_berat();
+    echo '<br>bunyinya.. ' . $robot2->get_suara();
+?>
